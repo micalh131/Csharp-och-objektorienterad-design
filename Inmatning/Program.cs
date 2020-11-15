123df20a55e801460ac11fe773beeefadb21fb1b
Michelle Alden <michelle.alden@live.se>
2020-11-15 14:39:34 +0100

    Added GradeBook, Inmatning och Ryggsäck

---
+++ Inmatning/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Inmatning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Mata in ditt namn:");
+            var inputName = Console.ReadLine();
+            Console.WriteLine($"Hej och välkommen {inputName}!");
+            Console.WriteLine("Mata in året du är född:");
+            var year = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Mata in månaden du är född:");
+            var month = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Mata in dagen du är född:");
+            var day = Convert.ToInt32(Console.ReadLine());
+
+
+            DateTime birthday = new DateTime(year, month, day);
+
+            Console.WriteLine($"Din födelsedag är: {birthday.Date}");
+
+            TimeSpan diff = DateTime.Now.Date - birthday.Date;
+            Console.WriteLine($"Du har left {diff.TotalDays} dagar på denna planet");
+
+        }
+    }
+}
---
+++ Ryggsäck/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ryggsäck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bag = new List<string>() { "Bulle", "Saft" };
+
+            while (true)
+            {
+
+
+                Console.WriteLine("Välkommen till ryggsäcken!");
+                Console.WriteLine("[1] Lägg till ett föremål");
+                Console.WriteLine("[2] Skriv ut innehållet");
+                Console.WriteLine("[3] Rense innehållet");
+                Console.WriteLine("[4] Avsluta");
+                Console.WriteLine("Välj:");
+                var number = Convert.ToInt32(Console.ReadLine());
+
+
+                if (number == 1)
+                {
+                    Console.WriteLine("Skriv föremål du vill lägga till:");
+                    var item = Console.ReadLine();
+                    bag.Add(item);
+                    Console.WriteLine("Föremålet är sparat");
+
+                }
+                else if (number == 2)
+                {
+                    foreach (var item in bag)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+                else if (number == 3)
+                {
+                    bag.Clear();
+                    Console.WriteLine("Ryggsäcken är nu tom");
+                }
+                else if (number == 4)
+                {
+                    return;
+                }
+
+            }
+
+        }
+    }
+}
